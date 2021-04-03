107c34f59d8ea1ababd6b26a1c9eb699cb59888f
benedikt <forcemaybeyouwith@gmail.com>
2021-04-03 23:06:51 +0500

    Удалил конфиг

--- configuration.js
+++
@@ -1,6 +0,0 @@
-
-
-module.exports = {
-  qnaStringConnection: "mongodb://localhost:27017/api",
-  TOKEN_SECRET: "40cac08832e290daa8d793b1f1038e848b7c8f7921f23a3767defb1831657e47987a723932004bd481786735a324ed862d87be6a8f397e6fd79e5eaaf0fd3ad1",
-}
